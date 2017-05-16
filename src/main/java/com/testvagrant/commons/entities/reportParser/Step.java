0e1bec2db608c9f967300bbfe91d84bdcfc67b61
Sukesh Kumar <sukeshkumar@Sukeshs-MacBook-Pro.local>
2017-05-16 15:10:41 +0530

    adding error_message to steps

--- src/main/java/com/testvagrant/commons/entities/reportParser/Step.java
+++ src/main/java/com/testvagrant/commons/entities/reportParser/Step.java
@@ -5,6 +5,15 @@ public class Step {
     private String status;
     private String keyword;
     private String name;
+    private String error_message;
+
+    public String getError_message() {
+        return error_message;
+    }
+
+    public void setError_message(String error_message) {
+        this.error_message = error_message;
+    }
 
     public String getStatus() {
         return status;
