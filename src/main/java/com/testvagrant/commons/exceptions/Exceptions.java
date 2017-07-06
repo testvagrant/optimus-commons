b932691fbb375b8d0bc0ae9b4f1201bcf030869b
krishnb <kbandame@gmail.com>
2017-07-06 12:36:53 +0530

    Added Exceptions.class

---
+++ src/main/java/com/testvagrant/commons/exceptions/Exceptions.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2017.  TestVagrant Technologies
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.testvagrant.commons.exceptions;
+
+
+/**
+ * Created by abhishek on 14/06/17.
+ */
+public class Exceptions {
+    private String stacktrace;
+    private String activityName;
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public String getStacktrace() {
+        return stacktrace;
+    }
+
+    public void setStacktrace(String stacktrace) {
+        this.stacktrace = stacktrace;
+    }
+}
